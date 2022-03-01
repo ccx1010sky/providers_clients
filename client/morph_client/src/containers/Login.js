145f3d95f60c11496811dc0a4449dfba687830fc
ccx1010sky <ccx1010@gmail.com>
2022-03-01 20:38:21 +0000

    login page

--- client/morph_client/src/containers/Login.js
+++ client/morph_client/src/containers/Login.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <h2>Log In</h2>
+     
+      <form>
+        <label>Email: </label>
+        <input type="text" />
 
-export default Login
+        <br />
+        <label>Password </label>
+        <input type="text" />
+
+        <br />
+        <input type="submit" />
+      </form>
+
+      <div>
+        Need an account?
+        <Link to="/signup">Sign Up</Link>
+      </div>
+    </>
+  );
+};
+
+export default Login;
