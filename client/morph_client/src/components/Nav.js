5933db1575ae69cbfbd68c4b2a9e7b47d06b03f7
ccx1010sky <ccx1010@gmail.com>
2022-03-01 16:53:48 +0000

    navData.js and nav.js

--- client/morph_client/src/components/Nav.js
+++ client/morph_client/src/components/Nav.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import react from "react";
+import { NavData } from "./NavData";
+import { useNavigate } from "react-router-dom";
 
-const Nav = () => {
+
+
+const Nav = ({}) => {
+  const navigate = useNavigate();
+
   return (
-    <div>Nav</div>
-  )
-}
+    <>
+      <nav className="nav-menu">
+        <ul className="nav-menu-items">
+          {NavData.map((item, index) => {
+            return (
+              <li
+                onClick={() => item.onClick(navigate)}
+                key={index}
+                className={item.cName}
+              >
+                <img className="navIcon" src={item.icon} />
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </>
+  );
+};
 
-export default Nav
+export default Nav;
---
+++ client/morph_client/src/components/NavData.js
@@ -0,0 +1,35 @@
+
+
+export const NavData = [
+  {
+    onClick: (navigate) => navigate("/"),
+    icon: logo,
+    cName: "nav-text",
+  },
+  {
+    onClick: (navigate) => navigate("/Login"),
+    icon: login,
+    cName: "nav-text",
+  },
+  {
+    onClick: (navigate) => navigate("/signup"),
+    icon: signup,
+    cName: "nav-text",
+  },
+  {
+    onClick: (navigate) => navigate("/provider"),
+    icon: provider,
+    cName: "nav-text",
+  },
+  {
+    onClick: (navigate) => navigate("/client"),
+    icon: client,
+    cName: "nav-text",
+  },
+];
+
+
+
+
+
+
