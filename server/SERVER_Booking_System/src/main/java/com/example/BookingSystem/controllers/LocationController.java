436c6da8b8113fc6c2355d0635c3f79df849a880
petermordaunt <peetfire@hotmail.com>
2022-03-07 12:21:15 +0000

    function - LocationController - added full CRUD function. Testing - Tested full CRUD function in Insomnia.

--- server/SERVER_Booking_System/src/main/java/com/example/BookingSystem/controllers/LocationController.java
+++ server/SERVER_Booking_System/src/main/java/com/example/BookingSystem/controllers/LocationController.java
@@ -5,9 +5,7 @@ import com.example.BookingSystem.repositories.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,16 +21,40 @@ public class LocationController {
     }
 
     //SHOW
-
+    @GetMapping(value = "/locations/{id}")
+    public ResponseEntity getLocation(@PathVariable Long id){
+        return new ResponseEntity(locationRepository.findById(id), HttpStatus.OK);
+    }
 
     // CREATE
-
+    @PostMapping(value = "/locations")
+    public ResponseEntity<Location> createLocation(@RequestBody Location location){
+        locationRepository.save(location);
+        return new ResponseEntity<>(location, HttpStatus.CREATED);
+    }
 
     // UPDATE
-
+    @PutMapping(value = "/locations/{id}")
+    public ResponseEntity<Location> updateLocation(@RequestBody Location location, @PathVariable Long id){
+        Location locationToUpdate = locationRepository.findById(id).get();
+        locationToUpdate.setName(location.getName());
+        locationToUpdate.setAddress(location.getAddress());
+        locationToUpdate.setEmail(location.getEmail());
+        locationToUpdate.setPhoneNumber(location.getPhoneNumber());
+        locationToUpdate.setRooms(location.getRooms());
+        locationToUpdate.setProviders(location.getProviders());
+        locationToUpdate.setAdmins(location.getAdmins());
+        locationToUpdate.setAppointments(location.getAppointments());
+        locationRepository.save(locationToUpdate);
+        return new ResponseEntity<>(locationToUpdate, HttpStatus.OK);
+    }
 
     // DESTROY
-
+    @DeleteMapping(value = "/locations/{id}")
+    public ResponseEntity<Long> destroyLocation(@PathVariable Long id){
+        locationRepository.deleteById(id);
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
 
 
     // Custom Routes
