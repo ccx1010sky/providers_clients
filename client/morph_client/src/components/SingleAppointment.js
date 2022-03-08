f07ab896102a6a0c0f6d0e4daa910d16b9ed24ee
peterwilliams <peterkennethwilliams@gmail.com>
2022-03-08 13:35:31 +0000

    single appointment view template added

--- client/morph_client/src/components/SingleAppointment.js
+++ client/morph_client/src/components/SingleAppointment.js
@@ -1,115 +1,133 @@
-// import { TextField, Button} from "@mui/material";
-// import * as React from "react";
-// import {Box }from "@mui/material/Box";
-// // import TextField from ‘@mui/material/TextField’;
+import { TextField, Button} from "@mui/material";
+import * as React from "react";
+import Box from '@mui/material/Box';
+    // import TextField from '@mui/material/TextField';
+    
+export default function SingleAppointment({singleAppointmentData}) {
+return (
+    <Box
+    component="form"
+    sx={{
+        boxShadow: 10, // theme.shadows[1]
+        color: "primary.main", // theme.palette.primary.main
+        m: 7, // margin: theme.spacing(1)
+        p: {
+        xs: 2, // [theme.breakpoints.up('xs')]: { padding: theme.spacing(1) }
+        },
+        zIndex: "tooltip", // theme.zIndex.tooltip
+        minWidth: 100,
+        "& .MuiTextField-root": { m: 1, width: "20ch" },
+    }}
+    noValidate
+    autoComplete="off"
 
+    // className="provider-container"
+    >
+    <div className="display-panel">
+        <div>
+        <h2>Appointment Details</h2>
+        </div>
+        <div>
+        <TextField
+            sx={{
+            boxShadow: 5, // theme.shadows[1]
+            color: "secondary.main", // theme.palette.primary.main
+            borderRadius: 2,
+            m: 0.5, // margin: theme.spacing(1)
+            p: {
+                xs: 0.3, // [theme.breakpoints.up('xs')]: { padding: theme.spacing(1) }
+            },
+            }}
+            label="Client name"
+            placeholder="data"
+        >
+            data goes here
+        </TextField>
+        </div>
+        <div>
+        <TextField
+            sx={{
+            boxShadow: 5, // theme.shadows[1]
+            color: "secondary.main", // theme.palette.primary.main
+            borderRadius: 2,
+            m: 0.5, // margin: theme.spacing(1)
+            p: {
+                xs: 0.3, // [theme.breakpoints.up('xs')]: { padding: theme.spacing(1) }
+            },
+            }}
+            label="Client name"
+        >
+            data goes here
+        </TextField>
+        </div>
+        <div>
+        <TextField
+            sx={{
+            boxShadow: 5, // theme.shadows[1]
+            color: "secondary.main", // theme.palette.primary.main
+            borderRadius: 2,
+            m: 0.5, // margin: theme.spacing(1)
+            p: {
+                xs: 0.3, // [theme.breakpoints.up('xs')]: { padding: theme.spacing(1) }
+            },
+            }}
+            label="Date"
+            placeholder="data"
+        >
+            data goes here
+        </TextField>
+        </div>
+        <div>
+        <TextField
+            sx={{
+            boxShadow: 5, // theme.shadows[1]
+            color: "secondary.main", // theme.palette.primary.main
+            borderRadius: 2,
+            m: 0.5, // margin: theme.spacing(1)
+            p: {
+                xs: 0.3, // [theme.breakpoints.up('xs')]: { padding: theme.spacing(1) }
+            },
+            }}
+            label="Start"
+            placeholder="data"
+        >
+            data goes here
+        </TextField>
 
-// export default function SingleAppointment({singleApppointmentData }) {
-//   return (
-//       <>
-//     <Box
-//       component=“form”
-//       sx={{
-//         boxShadow: 10, // theme.shadows[1
-//         color: “primary.main”, // theme.palette.primary.main
-//         m: 7, // margin: theme.spacing(1)
-//         p: {
-//           xs: 2, // [theme.breakpoints.up(‘xs’)]: { padding: theme.spacing(1) }
-//         },
-//         zIndex: “tooltip”, // theme.zIndex.tooltip
-//         minWidth: 100,
-//         “& .MuiTextField-root”: { m: 1, width: “20ch” },
-//       }}
-//       noValidate
-//       autoComplete=“off”
-//       // className=“provider-container”
-//     >
-//       <div className=“display-panel”>
-//         <div>
-//           <h2>Appointment Details</h2>
-//         </div>
-//         <div>
-//           <TextField
-//             sx={{
-//               boxShadow: 5, // theme.shadows[1]
-//               color: “secondary.main”, // theme.palette.primary.main
-//               borderRadius: 2,
-//               m: 0.5, // margin: theme.spacing(1)
-//               p: {
-//                 xs: 0.3, // [theme.breakpoints.up(‘xs’)]: { padding: theme.spacing(1) }
-//               },
-//             }}
-//             label=“Client name”
-//             placeholder=“data”
-//           >
-//             data goes here
-//           </TextField>
-//         </div>
-//         <div>
-//           <TextField
-//             sx={{
-//               boxShadow: 5, // theme.shadows[1]
-//               color: “secondary.main”, // theme.palette.primary.main
-//               borderRadius: 2,
-//               m: 0.5, // margin: theme.spacing(1)
-//               p: {
-//                 xs: 0.3, // [theme.breakpoints.up(‘xs’)]: { padding: theme.spacing(1) }
-//               },
-//             }}
-//             label=“Date”
-//             placeholder=“data”
-//           >
-//             data goes here
-//           </TextField>
-//         </div>
-//         <div>
-//           <TextField
-//             sx={{
-//               boxShadow: 5, // theme.shadows[1]
-//               color: “secondary.main”, // theme.palette.primary.main
-//               borderRadius: 2,
-//               m: 0.5, // margin: theme.spacing(1)
-//               p: {
-//                 xs: 0.3, // [theme.breakpoints.up(‘xs’)]: { padding: theme.spacing(1) }
-//               },
-//             }}
-//             label=“Start”
-//             placeholder=“data”
-//           >
-//             data goes here
-//           </TextField>
-//           <TextField
-//             sx={{
-//               boxShadow: 5, // theme.shadows[1]
-//               color: “secondary.main”, // theme.palette.primary.main
-//               borderRadius: 2,
-//               m: 0.5, // margin: theme.spacing(1)
-//               p: {
-//                 xs: 0.3, // [theme.breakpoints.up(‘xs’)]: { padding: theme.spacing(1) }
-//               },
-//             }}
-//             label=“End”
-//             placeholder=“data”
-//           >
-//             data goes here
-//           </TextField>
-//         </div>
-//         <div>
-//           <TextField label=“Provider” placeholder=“data”>
-//             data goes here
-//           </TextField>
-//         </div>
-//         <div>
-//           <TextField label=“Location” placeholder=“data”>
-//             data goes here
-//           </TextField>
-//         </div>
-//       </div>
-//       <Button variant=“contained”>Edit</Button>
-//     </Box>
-//     </>
-//   );
-// }
+        <TextField
+            sx={{
+            boxShadow: 5, // theme.shadows[1]
+            color: "secondary.main", // theme.palette.primary.main
+            borderRadius: 2,
+            m: 0.5, // margin: theme.spacing(1)
+            p: {
+                xs: 0.3, // [theme.breakpoints.up('xs')]: { padding: theme.spacing(1) }
+            },
+
+            }}
+            label="End"
+            placeholder="data"
+        >
+            data goes here
+        </TextField>
+        </div>
+
+        <div>
+        <TextField label="Provider" placeholder="data">
+            data goes here
+        </TextField>
+        </div>
+        <div>
+        <TextField label="Location" placeholder="data">
+            data goes here
+        </TextField>
+        </div>
+    </div>
+    <Button variant="contained">Edit</Button>
+    </Box>
+);
+}
+
 
 
 
